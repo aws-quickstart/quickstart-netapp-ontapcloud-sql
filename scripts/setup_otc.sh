3e6981f679bdbbabb3bd79c033f382c306f05bce
Brandon Moller <molleb@amazon.com>
2019-01-07 13:29:28 -0600

    Update ONTAP Cloud Manager to 3.5
    - Updated setup_otc.sh config script with partner-provided changes
    - Fixed formatting for the Cloud Manager template
    - Fixed property type errors in template linting
    - Added QSS3BucketName parameter to taskcat parameters

--- scripts/setup_otc.sh
+++ scripts/setup_otc.sh
@@ -43,15 +43,15 @@ chmod +x /usr/bin/jq
 
 function waitForAction
 {
-  curl http://localhost/occm/api/audit?workingEnvironmentId=${1} -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt | jq -r .[${3}].status > /tmp/temp.txt
+  curl http://localhost/occm/api/audit?workingEnvironmentId=${1} -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" | jq -r .[${3}].status > /tmp/temp.txt
   test=`cat /tmp/temp.txt`
   if [ ${test} = null ] ; then
 	sleep ${2}
 	waitForAction ${1} ${2} ${3}
   fi
-  while [ ${test} = Received ] || [ ${test} = null ] ; do sleep ${2};curl http://localhost/occm/api/audit?workingEnvironmentId=${1} -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt | jq -r .[${3}].status > /tmp/temp.txt;test=`cat /tmp/temp.txt`; done
+  while [ ${test} = Received ] || [ ${test} = null ] ; do sleep ${2};curl http://localhost/occm/api/audit?workingEnvironmentId=${1} -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" | jq -r .[${3}].status > /tmp/temp.txt;test=`cat /tmp/temp.txt`; done
   if [ ${test} = Failed ] ; then
-	  curl http://localhost/occm/api/audit?workingEnvironmentId=${1} -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt | jq -r .[${3}] > /tmp/temp.txt
+	  curl http://localhost/occm/api/audit?workingEnvironmentId=${1} -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" | jq -r .[${3}] > /tmp/temp.txt
 	  errorMessage=`cat /tmp/temp.txt| jq -r .errorMessage`
 	  actionName=`cat /tmp/temp.txt| jq -r .actionName`
 	  echo "Action: $actionName failed due to: $errorMessage" > /tmp/occmError.txt
@@ -61,19 +61,32 @@ function waitForAction
 
 dataAccessCidr=`aws ec2 describe-subnets --region $region --subnet-id $subnetID | jq -r '.Subnets[0].CidrBlock'`
 sleep 60
+## Register the OCCM
+curl  http://localhost/occm/api/occm/setup-portal/register --header 'Referer:AWSQS1' -X POST
+sleep 10
+## Get Auth0 details
+curl -X GET http://localhost/occm/api/occm/system/support-services --header 'Referer:AWSQS1' |jq -r .portalService.auth0Information > info.txt
+clientId=`cat info.txt|jq -r .clientId`
+domain=`cat info.txt|jq -r .domain`
+audience=`cat info.txt|jq -r .audience`
+token="Bearer "
+##Get token
+curl  https://$domain/oauth/token -X POST --header "Content-Type:application/json" --data '{"grant_type": "password","username": "'${adminEmail}'","password": "'${adminPassword}'","audience": "'${audience}'","scope": "profile","client_id": "'${clientId}'"}' | jq -r .access_token > token.txt
+token+=`cat token.txt`
 ## Setup Cloud Manager
-curl http://localhost/occm/api/occm/setup/init -X POST --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --data '{ "proxyUrl": { "uri": "" }, "userRequest":{  "email": "'${adminEmail}'","lastName": "admin", "firstName":"admin","accessKey": "nokeys","roleId": "Role-1","secretKey": "nokeys","password": "'${adminPassword}'"  }, "site": "'${customer}_site'", "company": "'${customer}'","tenantRequest": { "name": "'${customer}_tenant'", "description": "", "costCenter": "", "nssKeys": {} }}'
+curl  http://localhost/occm/api/occm/setup-portal/init -X POST --header "Content-Type:application/json" --header 'Referer:AWSQS1' --header "Authorization:$token" --data '{"adminUser": {"email": "'${adminEmail}'"},"company": "'${customer}'","site": "'${customer}_site'"}'
 sleep 40
 until sudo wget http://localhost/occmui > /dev/null 2>&1; do sudo wget http://localhost > /dev/null 2>&1 ; done
 sleep 60
-## Authenticate to Cloud Manager
-curl http://localhost/occm/api/auth/login --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --data '{"email":"'${adminEmail}'","password":"'${adminPassword}'"}' --cookie-jar cookies.txt
+## Get Cloud Account id
+curl -X GET http://localhost/occm/api/accounts --header 'Referer:AWSQS1' --header "Authorization: $token"  |jq -r '.awsAccounts[0]'.publicId > temp.txt
+awsAccountId=`cat temp.txt`
 sleep 5
 ## Get the Tenant ID so we can create the ONTAP Cloud system in that Cloud Manager Tenant
-tenantId=`curl http://localhost/occm/api/tenants -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt | jq -r .[0].publicId`
+tenantId=`curl http://localhost/occm/api/tenants -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" | jq -r .[0].publicId`
 ## Create a ONTAP Cloud working env
 name=$(tr '-' '_' <<< ${name:0:30})
-curl http://localhost/occm/api/vsa/working-environments -X POST --cookie cookies.txt --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --data '{"name":"'${name}_OTC'","tenantId":"'${tenantId}'","region":"'${region}'","subnetId":"'${subnetID}'","ebsVolumeType":"gp2","ebsVolumeSize": {"size": 1, "unit": "TB"},"dataEncryptionType":"AWS","ontapEncryptionParameters":null,"skipSnapshots": "true","svmPassword":"'${svmPassword}'","vpcId":"'${vpcID}'","vsaMetadata":{"platformLicense":null,"ontapVersion":"latest","useLatestVersion": true,"licenseType":"cot-standard-paygo","instanceType":"r4.xlarge"}}' > /tmp/createVSA.txt
+curl http://localhost/occm/api/vsa/working-environments -X POST --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" --data '{"name":"'${name}_OTC'","tenantId":"'${tenantId}'","region":"'${region}'","subnetId":"'${subnetID}'","ebsVolumeType":"gp2","ebsVolumeSize": {"size": 1, "unit": "TB"},"dataEncryptionType":"AWS","ontapEncryptionParameters":null,"skipSnapshots": "true","svmPassword":"'${svmPassword}'","vpcId":"'${vpcID}'","cloudProviderAccount": "'${awsAccountId}'","vsaMetadata":{"platformLicense":null,"ontapVersion":"latest","useLatestVersion": true,"licenseType":"cot-standard-paygo","instanceType":"r4.xlarge"}}' > /tmp/createVSA.txt
 sourceVsaPublicId=`cat /tmp/createVSA.txt| jq -r .publicId`
 if [ ${sourceVsaPublicId} = null ] ; then
   message=`cat /tmp/createVSA.txt| jq -r .message`
@@ -84,13 +97,13 @@ sleep 2
 ## Check SRC VSA
 waitForAction ${sourceVsaPublicId} 60 1
 ## grab the Cluster managment LIF IP address
-clusterLif=`curl 'http://localhost/occm/api/vsa/working-environments/'${sourceVsaPublicId}'?fields=clusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt |jq -r .clusterProperties.lifs |grep "Cluster Management" -a2|head -1|cut -f4 -d '"'`
+clusterLif=`curl 'http://localhost/occm/api/vsa/working-environments/'${sourceVsaPublicId}'?fields=clusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" |jq -r .clusterProperties.lifs |grep "Cluster Management" -a2|head -1|cut -f4 -d '"'`
 echo "${clusterLif}" > /tmp/clusterLif.txt
 ## grab the iSCSI data LIF IP address
-dataLif=`curl 'http://localhost/occm/api/vsa/working-environments/'${sourceVsaPublicId}'?fields=clusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt |jq -r .clusterProperties.lifs |grep iscsi -a4|head -1|cut -f4 -d '"'`
+dataLif=`curl 'http://localhost/occm/api/vsa/working-environments/'${sourceVsaPublicId}'?fields=clusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" |jq -r .clusterProperties.lifs |grep iscsi -a4|head -1|cut -f4 -d '"'`
 echo "${dataLif}" > /tmp/iscsiLif.txt
 ## grab the NFS and CIFS data LIF IP address
-dataLif2=`curl 'http://localhost/occm/api/vsa/working-environments/'${sourceVsaPublicId}'?fields=clusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --cookie cookies.txt |jq -r .clusterProperties.lifs |grep nfs -a4|head -1|cut -f4 -d '"'`
+dataLif2=`curl 'http://localhost/occm/api/vsa/working-environments/'${sourceVsaPublicId}'?fields=clusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AWSQS1' --header "Authorization:$token" |jq -r .clusterProperties.lifs |grep nfs -a4|head -1|cut -f4 -d '"'`
 echo "${dataLif2}" > /tmp/nasLif.txt
 
 # Remove passwords from files
